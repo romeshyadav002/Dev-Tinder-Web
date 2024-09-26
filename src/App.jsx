50ba505660c30f73d16249a6ffb706ac413e0458
Romesh Yadav <romesh.yadav@jumbaya.com>
2024-09-26 15:06:23 +0530

    implemented navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-function App() {
+import NavBar from './components/NavBar';
 
+function App() {
   return (
     <>
-      
+      <NavBar />
       <h1>DEV Tinder</h1>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
