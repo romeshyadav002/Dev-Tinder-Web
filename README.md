5ea56f91b8465e885e517899d84d5c242815d71b
Romesh Yadav <romesh.yadav@jumbaya.com>
2024-09-26 20:44:44 +0530

    implemented browserRouter

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 - removed unnecessary code and created hello world app
 - Install tailwind css
 - Install Daisy UI
+- implemented navbar.jsx
+- installed react-router-dom
+- create browserRouter > Routes > Route=/Body > RouteChildren
+- Create an Outlet in your body component
+- Create a footer component
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import NavBar from './components/NavBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Body from './components/Body';
+import Login from './components/Login';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <>
-      <NavBar />
-      <h1>DEV Tinder</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
