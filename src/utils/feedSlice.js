33d5709a30610c07ce053cbe29a4a2241397f434
Romesh Yadav <romesh.yadav@jumbaya.com>
2024-09-28 13:22:05 +0530

    implemented feed, connection, request slice

--- src/utils/feedSlice.js
+++ src/utils/feedSlice.js
@@ -4,11 +4,15 @@ const feedSlice = createSlice({
   name: 'feed',
   initialState: null,
   reducers: {
-    addFeed: (state, action) => action.payload,
-    removeFeed: (state, action) => nul,
+    addFeed: (state, action) => {
+      return action.payload;
+    },
+    removeUserFromFeed: (state, action) => {
+      const newFeed = state.filter((user) => user._id !== action.payload);
+      return newFeed;
+    },
   },
 });
 
-export const { addFeed, removeFeed } = feedSlice.actions;
-
+export const { addFeed, removeUserFromFeed } = feedSlice.actions;
 export default feedSlice.reducer;
